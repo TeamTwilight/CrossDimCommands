76bbf9d6d0aa34c5e22659b0756eae40e475ea53
jodi / jodlodi <jodlodi090@gmail.com>
2022-07-30 14:41:07 +0200

    Port to 1.17.1

--- src/main/java/com/crossdimcommands/CrossDimCommands.java
+++ src/main/java/com/crossdimcommands/CrossDimCommands.java
@@ -2,19 +2,23 @@ package com.crossdimcommands;
 
 import com.mojang.brigadier.ParseResults;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.logging.LogUtils;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.arguments.coordinates.Vec2Argument;
+import net.minecraft.commands.arguments.coordinates.Coordinates;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.commands.WorldBorderCommand;
-import net.minecraft.tags.TagKey;
+import net.minecraft.tags.StaticTagHelper;
+import net.minecraft.tags.StaticTags;
 import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -23,14 +27,16 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @Mod.EventBusSubscriber(modid = CrossDimCommands.MOD_ID)
 public class CrossDimCommands {
     public static final String MOD_ID = "crossdimcommands";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public CrossDimCommands() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    public static final TagKey<DimensionType> WEATHER_COMMAND_BLACKLIST = TagKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(MOD_ID, "weather_command_blacklist"));
-    public static final TagKey<DimensionType> WORLDBORDER_COMMAND_BLACKLIST = TagKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(MOD_ID, "worldborder_command_blacklist"));
+    protected static final StaticTagHelper<DimensionType> HELPER = StaticTags.create(Registry.DIMENSION_TYPE_REGISTRY, "tags/dimension_type");
+
+    public static final Tags.IOptionalNamedTag<DimensionType> WEATHER_COMMAND_BLACKLIST = HELPER.createOptional(new ResourceLocation(MOD_ID, "weather_command_blacklist"), null);
+    public static final Tags.IOptionalNamedTag<DimensionType> WORLDBORDER_COMMAND_BLACKLIST = HELPER.createOptional(new ResourceLocation(MOD_ID, "worldborder_command_blacklist"), null);
 
     @SubscribeEvent
     public static void weatherCommandListener(CommandEvent event) {
@@ -39,18 +45,25 @@ public class CrossDimCommands {
         CommandSourceStack sourceStack = parseResults.getContext().getSource();
 
         if (sourceStack != null) {
-            if ((command[0].equals("/weather") || command[0].equals("weather")) && !sourceStack.getLevel().dimensionTypeRegistration().is(WEATHER_COMMAND_BLACKLIST)) {
+            if ((command[0].equals("/weather") || command[0].equals("weather")) && !WEATHER_COMMAND_BLACKLIST.contains(sourceStack.getLevel().dimensionType())) {
                 switch (command[1]) {
                     case "clear" -> sourceStack.getLevel().getServer().overworld().setWeatherParameters(6000, 0, false, false);
                     case "rain" -> sourceStack.getLevel().getServer().overworld().setWeatherParameters(0, 6000, true, false);
                     case "thunder" -> sourceStack.getLevel().getServer().overworld().setWeatherParameters(0, 6000, true, true);
                 }
-            } else if ((command[0].equals("/worldborder") || command[0].equals("worldborder")) && command[1].equals("center") && !sourceStack.getLevel().dimensionTypeRegistration().is(WORLDBORDER_COMMAND_BLACKLIST)) {
-                try {
-                    WorldBorderCommand.setCenter(sourceStack, Vec2Argument.getVec2(parseResults.getContext().build(parseResults.getReader().getRead()), "pos").scale((float)sourceStack.getLevel().dimensionType().coordinateScale()));
-                    event.setCanceled(true);
-                } catch (CommandSyntaxException e) {
-                    e.printStackTrace();
+            } else if (command[0].equals("/worldborder") || command[0].equals("worldborder") && !WORLDBORDER_COMMAND_BLACKLIST.contains(sourceStack.getLevel().dimensionType())) {
+                ParseResults<CommandSourceStack> newParseResults = new ParseResults<CommandSourceStack>(parseResults.getContext().withSource(parseResults.getContext().getSource().withLevel(sourceStack.getLevel().getServer().overworld())), parseResults.getReader(), parseResults.getExceptions());
+                event.setParseResults(newParseResults);
+
+                if (command[1].equals("center")) {
+                    try {
+                        event.setCanceled(true);
+                        Vec3 pos = newParseResults.getContext().build(newParseResults.getReader().getRead()).getArgument("pos", Coordinates.class).getPosition(sourceStack);
+                        double scale = sourceStack.getLevel().dimensionType().coordinateScale();
+                        WorldBorderCommand.setCenter(newParseResults.getContext().getSource(), new Vec2((float)(pos.x * scale), (float)(pos.z * scale)));
+                    } catch (CommandSyntaxException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
